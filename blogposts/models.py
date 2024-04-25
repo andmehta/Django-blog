7eff93d026831d9b5549e894029a7a95315a5167
andrewmehta <andmehta@icloud.com>
2024-04-25 14:14:19 -0500

    create blogposts app and BlogPost model

---
+++ blogposts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class BlogPost(models.Model):
+    """Basic model for a Blog Post"""
+    title = models.CharField(null=True, max_length=50)
+    content = models.CharField()
+    # Since it's against best practices to delete users (should always deactivate) The decision on what to do on_delete
+    #  Doesn't REALLY matter. But I think just having an author-less blogpost makes the most sense
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
