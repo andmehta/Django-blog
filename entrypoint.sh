2108d4d421f1cf3b74d6f43bc4d51349c56e5ca1
andrewmehta <andmehta@icloud.com>
2024-04-25 19:56:40 -0500

    clarifying comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "migrating database"
 python manage.py migrate
 
-# password comes from secrets/superuserpassword.txt
+# password comes from environment variable DJANGO_SUPERUSER_PASSWORD
 echo "trying to create the superuser"
 python manage.py createsuperuser --noinput --username admin1 --email admin@example.com
 
